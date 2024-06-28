5856b10fff220a4fba65bd27be68995280f0ca6a
johnsistem <johnnyqm5@gmail.com>
2024-06-28 13:45:36 -0600

    se trabajo hasta el capitulo 5

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,6 +4,7 @@ import { lusitana } from '@/app/ui/fonts';
 export default function AcmeLogo() {
   return (
     <div
+     // className={` flex flex-row items-center leading-none text-white`}
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
